82e99d3e9af4fbb9f2fbedc214b6e825165cd715
BananaBananaLimon <denis.matsyura@yandex.ua>
2018-07-22 18:39:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AksonBank());
+            Console.ReadKey();
+        }
+
+        private static String AksonBank()
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            String Response = wc.DownloadString("http://www.atbmarket.com/ru/hot/akcii/economy/");
+            //id="conv0" value="(64.25)" 60</span>
+            String Rate = System.Text.RegularExpressions.Regex.Match(Response, @"<div class=""promo_price"">([0-9]+)<span>([0-9]+)").Groups[1].Value;
+            return "АТБ: " + Rate + " р. \r\n";
+        }
+
+        private static String RSBank()
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            String Response = wc.DownloadString("http://www.rshb.ru/branches/saratov/");
+            File.WriteAllText(@"C:\text.txt", Response);
+            String Rate = System.Text.RegularExpressions.Regex.Match(Response, @"<td>([0-9]+\.[0-9]+)</td>").Groups[1].Value;
+            return "Россельхозбанк: " + Rate + " р. \r\n";
+        }
+    }
+}
